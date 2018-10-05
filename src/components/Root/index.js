a2b4b8eb03149a70e59a8bf72820acb31fecee19
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-05 23:42:56 +0300

    Update Root

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Router} from 'react-static';
 import {hot} from 'react-hot-loader';
 import Routes from 'react-static-routes';
-import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
-import 'lazysizes';
 import '../../assets/fonts/montserrat';
 import './index.scss';
+
+if (typeof window !== 'undefined') {
+    /* eslint-disable global-require */
+    require('lazysizes/plugins/unveilhooks/ls.unveilhooks');
+    require('lazysizes');
+    /* eslint-enable global-require */
+}
 
 const App = () => (
     <Router>
