1a942278e2b73904e9160927a7253d3df60d0aae
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-05 23:14:59 +0300

    Add dms images

---
+++ src/components/Picture/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Classnames from 'classnames';
+
+/**
+ * Renders Picture component
+ * @param {Object}
+ * @returns {JSX}
+ */
+const Picture = ({
+    src,
+    alt,
+    width,
+    height,
+    lazyload
+}) => {
+    const pictureClass = Classnames({
+        lazyload
+    });
+
+    return (
+        <img
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            className={pictureClass} />
+    );
+};
+
+Picture.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+    lazyload: PropTypes.bool
+};
+
+Picture.defaultProps = {
+    lazyload: true
+};
+
+export default Picture;
