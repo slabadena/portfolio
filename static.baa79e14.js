f7aa5881aef7b0d5ff7c6661313b894c924d9aca
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-07 14:28:33 +0300

    Updates

--- static.baa79e14.js
+++ static.baa79e14.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://slabadena.github.io/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 10);
@@ -2345,4 +2345,4 @@ module.exports = require("lazysizes");
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.7fd9ab40.js.map
+//# sourceMappingURL=static.baa79e14.js.map
